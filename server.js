0aed0c5b081f2b6d4a9af28147a6f3061aaa78d3
Florent33 <ferrand.florent@gmail.com>
2015-06-09 14:43:30 +0200

    commit server.js

--- server.js
+++ server.js
@@ -44,10 +44,8 @@ router.route('/bears')
 
     // create a bear (accessed at POST http://localhost:8080/api/bears)
     .post(function(req, res) {
-
         var bear = new Bear();      // create a new instance of the Bear model
         bear.name = req.body.name;  // set the bears name (comes from the request)
-
         // save the bear and check for errors
         bear.save(function(err) {
             if (err)
